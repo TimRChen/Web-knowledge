edde4243e1db8c08271252383f61b15f27d0a5d6
TimRChen <527410313@qq.com>
2017-05-05 20:47:53 +0800

    use ES6 Promise Object for ajax

--- ajax/README.md
+++ ajax/README.md
@@ -4,6 +4,9 @@
 
 ## 形式
 *   使用ES6 Class语法对ajax方法进行封装
+*   使用Promise对象对请求的数据进行回调传值
+    *   增加错误捕捉
+    *   完善回调处理流程，较之前相比更加简洁
 
 ## 使用
     首先实例化Ajax类，let ajax = new Ajax() 即可。Ajax类中没有定义静态方法。子类可继承。
@@ -13,4 +16,4 @@
 | ---- | ------- | ---- |
 | send | new Ajax().send() | 参数有`method` `url` `async` `callback` `data` |
 | callback | (resData) => { console.log(resData) } | 参数为`responseText`值，可指定任意形参 | 
-|调用形式: new Ajax().send(method, url, async, callback, data)| --- | --- |
+|调用形式: new Ajax().send(method, url, async, callback, data)| :---: | :---: |
--- ajax/ajax.js
+++ ajax/ajax.js
@@ -4,24 +4,44 @@ class Ajax  {
         this.xhr = xhr;
     }
 
-    send(method, url, async, callback, data) {
-    // send(options) {
-
-        // options = 
-
+    send(options) {
 
         let xhr = this.xhr;
 
-        xhr.onreadystatechange = () => {
-            // readyState: 0: init, 1: connect has set up, 2: recive request, 3: request.. , 4: request end, send response
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                // status: 200: OK,  404: Not Found Page
-                callback(xhr.responseText);
-            }
+        let opt = {
+            type: options.type || 'get',
+            url: options.url || '',
+            async: options.async || true,
+            dataType: options.dataType || 'json',
+            questring: options.questring || ''
         };
 
-        xhr.open(method, url, async);   
-        xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-        xhr.send(data);
+        return new Promise((resolve, reject) => {
+
+            xhr.open(opt.type, opt.url, opt.async);
+
+            xhr.onreadystatechange = () => {
+                // readyState: 0: init, 1: connect has set up, 2: recive request, 3: request.. , 4: request end, send response
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        // status: 200: OK,  404: Not Found Page
+                        if (opt.dataType === 'json') {
+                            const data = JSON.parse(xhr.responseText);
+                            resolve(data);
+                        }
+                    } else {
+                        reject(new Error(xhr.status || 'Server is fail.'));
+                    }
+                }
+            };
+
+            xhr.onerror = () => {
+                reject(new Error(xhr.status || 'Server is fail.'));
+            }
+
+            xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+            xhr.send(opt.questring);
+
+        });
     }
 }
